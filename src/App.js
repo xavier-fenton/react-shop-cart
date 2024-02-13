b198b13a084161377d9f39cd501aee6aa82eb47f
Xavier Fenton <xavierfenton99@gmail.com>
2024-02-14 09:27:40 +1300

    basic set up react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { create } from 'zustand'
+
+const useStore = create((set) => ({
+  count: 1,
+  inc: () => set((state) => ({ count: state.count + 1 })),
+}))
+
+function Counter() {
+  const { count, inc } = useStore()
+  return (
+    <div class="counter">
+      <span>{count}</span>
+      <button onClick={inc}>one up</button>
+    </div>
+  )
+}
+
 function App() {
-  return <div className="text-3xl font-bold underline">hello</div>
+  return (
+    <div>
+      <div>hello</div>
+      <div>hello</div>
+      <div>{Counter()}</div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/Components/Test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Test = () => {
+  return <div>Testing Page</div>
+}
+
+export default Test
