476066a33869f5d2bb4eaf30cb93a455586dcab5
Xavier Fenton <xavierfenton99@gmail.com>
2024-03-06 14:21:34 +1300

    rm if clause and add one more test

--- src/__tests__/cart.test.js
+++ src/__tests__/cart.test.js
@@ -9,22 +9,28 @@ import '@testing-library/jest-dom'
 // the component to test
 import Cart from '../Components/Cart'
 
-if (Cart) {
-  describe('Tests Cart component', () => {
-    test('That App loads and displays the cart section, given the state of the cart is filled and displays a total from the items', async () => {
-      render(<Cart state={'Cart'} />)
-      const totalId = screen.getByTestId('cart_total')
+describe('Test all within Cart component', () => {
+  // test 1
+  test('That App loads and displays the cart section, given the state of the cart is filled and displays a total from the items', async () => {
+    render(<Cart state={'Cart'} />)
+    const totalId = screen.getByTestId('cart_total')
 
-      expect(totalId).toBeInTheDocument()
-    })
-
-    test('That total is displayed, even when it updates. We expect a total cost to change if more is added to the cart', async () => {
-      render(<Cart state={'Cart'} />)
-      expect(screen.getByTestId('cart_total')).toHaveTextContent('Total: $')
-    })
-    test('Cart is empty', async () => {
-      render(<Cart state={'Created'} />)
-      expect(screen.getByTestId('cart_empty')).toBeInTheDocument()
-    })
+    expect(totalId).toBeInTheDocument()
   })
-}
+  // test 2
+  test('That total is displayed, even when it updates. We expect a total cost to change if more is added to the cart', async () => {
+    render(<Cart state={'Cart'} />)
+    expect(screen.getByTestId('cart_total')).toHaveTextContent('Total: $')
+  })
+  // test 3
+  test('Cart is empty', async () => {
+    render(<Cart state={'Created'} />)
+    expect(screen.getByTestId('cart_empty')).toBeInTheDocument()
+  })
+
+  test('Cart product displays all values required, and if state is not cart that it doesnt exist', async () => {
+    render(<Cart state={'Cart'} />)
+    const product_id = screen.getByTestId('product_test')
+    expect(product_id).toBeInTheDocument()
+  })
+})
