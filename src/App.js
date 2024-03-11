ffb4a66ae8eefc61dbc6f1aa6922f443aa2fee6a
Xavier Fenton <xavierfenton99@gmail.com>
2024-03-12 11:45:03 +1300

    reset: rm zustand features

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import Cart from './Components/Cart'
 import ProductCreator from './Components/ProductCreator'
-import Products from './Components/ProductsColumn'
 import ViewProducts from './Components/ViewProducts'
-import { useStore } from './store'
 import { productContext } from './shopContext'
 function App() {
-  //Global state for testing
-  // let findState = useStore((store) =>
-  //   store.products.find((products) => {
-  //     return products
-  //   })
-  // )
-  // const state = findState.state
-
   return (
     <div>
       <div className="flex flex-row justify-between">
